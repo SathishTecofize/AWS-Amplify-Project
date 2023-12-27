039b0344b106ba0e5dbcc59051d27c54e519149d
Sathish <sathish.k@tecofize.com>
2023-12-27 18:12:54 +0530

    testing

--- sample-app/src/App.js
+++ sample-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and AWS amplify ....
+          Learn React and AWS amplify testing !!!
         </a>
       </header>
     </div>
